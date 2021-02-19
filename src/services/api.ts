e3e180a6e8fe198448181d470f2c6210532920c5
Renan Rodrigo <rebarbos@redhat.com>
2021-02-19 11:32:09 +0100

    get API url from config file
    Signed-off-by: Renan Rodrigo <rebarbos@redhat.com>

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,8 @@
 import axios from 'axios';
+import config from './config';
 
 const api = axios.create({
-  baseURL: 'http://worldtimeapi.org/api/',
+  baseURL: config.apiUrl,
 });
 
 export default api;
