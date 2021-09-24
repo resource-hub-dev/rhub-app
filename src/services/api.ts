001fd911cae10b93ed9b9b8ff35b0d4f4ef09803
Charles Thao <cthao@redhat.com>
2021-09-24 11:15:25 -0400

    feat: allow CORS

--- src/services/api.ts
+++ src/services/api.ts
@@ -3,6 +3,11 @@ import config from './config';
 
 const api = axios.create({
   baseURL: config.apiUrl,
+  withCredentials: false,
+  headers: {
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET,PUT,POST,DELETE,PATCH,OPTIONS',
+  },
 });
 
 export default api;
