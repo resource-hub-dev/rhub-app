1938f842a829fd361eb300770f23e6d857cbec8c
Charles Thao <cthao@redhat.com>
2021-09-29 15:53:26 -0400

    feat: all cluster tables

---
+++ src/components/clusters/ClusterView.tsx
@@ -0,0 +1,195 @@
+import React from 'react';
+import { useSelector } from 'react-redux';
+import { Link, useParams } from 'react-router-dom';
+
+import {
+  Button,
+  Card,
+  CardBody,
+  CardTitle,
+  Spinner,
+} from '@patternfly/react-core';
+import { WarningTriangleIcon } from '@patternfly/react-icons';
+
+import { AppState } from '@store';
+
+import { Cluster, ClusterHost } from '@ducks/lab/cluster/types';
+
+import DataTable, { RowPair } from '@components/dataTable/DataTable';
+import { Quota } from '@ducks/lab/types';
+
+import ClusterTableUtilization from './ClusterTableUtilization';
+
+interface Props {
+  clusterViewType: 'user' | 'shared' | 'admin';
+}
+
+interface Params {
+  groupname: string;
+}
+
+const ClusterView: React.FC<Props> = ({ clusterViewType }: Props) => {
+  /* For shared clusters, we do not need to show owner, group, reservation and lifespan expiration dates:
+    1. owner and group are both the same for every week
+    2. they get rebuilt every weekend, so the rsvp and lifespan expirations are irrelevant
+    */
+  const { groupname } = useParams<Params>();
+
+  const columns =
+    clusterViewType === 'shared'
+      ? ['Name', 'Template', 'Region', 'Status']
+      : [
+          'Name',
+          'Owner',
+          'Template',
+          'Group',
+          'Region',
+          'Status',
+          'Reservation Expires',
+          'Lifespan Expires ',
+        ];
+  let rows: RowPair[] = [];
+  const clusters = useSelector((state: AppState) => state.cluster.data);
+  const loading = useSelector((state: AppState) => state.cluster.loading);
+  const generateCharts = (hosts: ClusterHost[], quota: Quota) => {
+    let num_vcpus = 0;
+    let ram_mb = 0;
+    let volumes_gb = 0;
+    hosts.forEach((host: ClusterHost) => {
+      num_vcpus += host.num_vcpus;
+      ram_mb += host.ram_mb;
+      volumes_gb += host.volumes_gb;
+    });
+    return (
+      <ClusterTableUtilization
+        cpuQuota={quota.num_vcpus}
+        ramQuotaMb={quota.ram_mb}
+        volumesMaxGb={quota.volumes_gb}
+        num_vcpus={num_vcpus}
+        ram_mb={ram_mb}
+        volumes_gb={volumes_gb}
+      />
+    );
+  };
+  if (loading) {
+    return (
+      <div>
+        <h3>Loading...</h3>
+        <Spinner />
+      </div>
+    );
+  }
+  const mapRows = (clusterEntries: Cluster[]) => {
+    if (clusterViewType === 'shared') {
+      return clusterEntries.map((item: Cluster) => ({
+        parent: [
+          <Link to={`/resources/quickcluster/clusters/${item.id}`}>
+            {String(item.name)}
+          </Link>,
+          item.template_id,
+          item.region_name,
+          item.status,
+        ],
+        child: item.hosts.length
+          ? [generateCharts(item.hosts, item.quota)]
+          : null,
+      }));
+    }
+    return clusterEntries.map((item: Cluster) => ({
+      parent: [
+        <Link
+          to={{
+            pathname: `/resources/quickcluster/clusters/${item.id}`,
+            // eslint-disable-next-line no-restricted-globals
+            state: { prevPath: location.pathname },
+          }}
+        >
+          {item.name}
+        </Link>,
+        item.user_name,
+        item.template_id,
+        item.group_name || '',
+        item.region_name,
+        item.status,
+        <Link
+          to={{
+            pathname: `/resources/quickcluster/clusters/${item.id}/extend`,
+            // eslint-disable-next-line no-restricted-globals
+            state: { prevPath: location.pathname },
+          }}
+        >
+          {(item.reservation_expiration &&
+            String(item.reservation_expiration.toLocaleString())) ||
+            ''}
+        </Link>,
+        clusterViewType === 'admin' ? (
+          <Link
+            to={{
+              pathname: `/resources/quickcluster/clusters/${item.id}/lifespan/extend`,
+              // eslint-disable-next-line no-restricted-globals
+              state: { prevPath: location.pathname },
+            }}
+          >
+            {(item.lifespan_expiration &&
+              String(item.lifespan_expiration.toLocaleString())) ||
+              ''}
+          </Link>
+        ) : (
+          (item.lifespan_expiration &&
+            String(item.lifespan_expiration.toLocaleString())) ||
+          ''
+        ),
+      ],
+      child: item.hosts.length
+        ? [generateCharts(item.hosts, item.quota)]
+        : null,
+    }));
+  };
+
+  if (Object.keys(clusters).length > 0) {
+    rows = mapRows(Object.values(clusters));
+  }
+  let title = '';
+
+  switch (clusterViewType) {
+    case 'user':
+      title = groupname ? `My Group's Clusters` : 'My Clusters:';
+      break;
+    case 'shared':
+      title = 'Shared Clusters';
+      break;
+    default:
+      title = 'All Clusters';
+      break;
+  }
+  return (
+    <Card isCompact>
+      <CardTitle>
+        {title}{' '}
+        {clusterViewType !== 'shared' && (
+          <Link
+            to={{
+              pathname: `/clusters/new`,
+              // eslint-disable-next-line no-restricted-globals
+              state: { prevPath: location.pathname },
+            }}
+          >
+            <Button variant="secondary">New Cluster</Button>
+          </Link>
+        )}
+      </CardTitle>
+      <CardBody>
+        {Object.keys(clusters).length === 0 ? (
+          <div className="no-cluster-card-body">
+            <WarningTriangleIcon width="45px" height="75px" />
+            <h1>No Clusters Available</h1>
+          </div>
+        ) : (
+          <DataTable columns={columns} rowPairs={rows} loading={loading} />
+        )}{' '}
+      </CardBody>
+    </Card>
+  );
+};
+
+export default ClusterView;
---
+++ src/components/clusters/SharedClusters.tsx
@@ -0,0 +1,62 @@
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+
+import {
+  PageSection,
+  PageSectionVariants,
+  Alert,
+  List,
+  ListItem,
+} from '@patternfly/react-core';
+import { AppState } from '@store';
+import { loadRequest as loadClusters } from '@ducks/lab/cluster/actions';
+
+import ClusterView from './ClusterView';
+import './Clusters.css';
+
+const SharedClusters: React.FC = () => {
+  const dispatch = useDispatch();
+
+  const token = useSelector((state: AppState) => state.user.current.token);
+  useEffect(() => {
+    dispatch(loadClusters('all', { user_name: 'sharedclusters' }));
+  }, [dispatch, token]);
+
+  const info = (
+    <Alert title="Purpose" isLiveRegion variant="info">
+      <div className="shared-clusters-info">
+        {`These clusters are meant to be used for reference. It would be
+        appreciated that you use these clusters rather than create your own
+        reference clusters in order to save resources. As these clusters are
+        meant to be shared, please don't alter them or otherwise run commands
+        that have the potential to break them for others.`}
+      </div>
+      <b>How to Use:</b>
+      <div className="shared-clusters-info">
+        There is no need to sign out/reserve these clusters, they can be used by
+        multiple users concurrently. Simply click on the cluster name you are
+        interested in to get the details, they are accessed the same as any
+        other cluster in QuickCluster. Do not place any data in these clusters,
+        they are automatically destroyed and re-created periodically, typically
+        during the weekend.
+      </div>
+      <b>Naming Convention Examples:</b>
+      <List>
+        <ListItem>
+          sharedocp48 --{'>'} Openshift 4.8 default QuickCluster installation
+        </ListItem>
+        <ListItem>
+          sharedocp39cns --{'>'} Openshift 3.9 with CNS installed
+        </ListItem>
+      </List>
+    </Alert>
+  );
+  return (
+    <PageSection variant={PageSectionVariants.light}>
+      <ClusterView clusterViewType="shared" />
+      {info}
+    </PageSection>
+  );
+};
+
+export default SharedClusters;
