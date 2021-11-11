b5ca7f2bb495dc12cbf888e398da8bcc9308a9e5
Charles Thao <cthao@redhat.com>
2021-11-11 14:13:25 -0500

    feat: Add Resources Dashboard Catalog

---
+++ src/components/ResourcesDashboard/ResourcesDashboard.tsx
@@ -0,0 +1,76 @@
+import {
+  PageSection,
+  TextContent,
+  Text,
+  PageSectionVariants,
+  Grid,
+  GridItem,
+} from '@patternfly/react-core';
+import React from 'react';
+import ResourcesDashboardSvg from 'assets/images/QuickCluster.svg';
+
+import './ResourcesDashboard.css';
+import ResourcesDashboardCard from './Card/ResourcesDashboardCard';
+
+const ResourcesDashboard: React.FC = () => {
+  const dashboardItems = [
+    {
+      logoLink: ResourcesDashboardSvg,
+      logoAlt: 'quickcluster',
+      body: (
+        <div>
+          {' '}
+          <Text component="p">
+            QuickCluster provides short-lived clusters with Red Hat products
+            preinstalled that could be created with a single click, perfect for
+            customer reproducers. It is the successor of QuickLab.
+          </Text>
+          <Text component="p" className="subtitle">
+            This service is hosted on OpenShift and powered by OpenStack and
+            Ansible Tower
+          </Text>
+        </div>
+      ),
+      footerLinks: [
+        {
+          link: '/resources/quickcluster/clusters',
+          title: 'View Your Clusters',
+        },
+        {
+          link: '/resources/quickcluster/shared',
+          title: 'Explore OpenShift Shared Clusters',
+        },
+      ],
+    },
+  ];
+
+  return (
+    <>
+      <PageSection variant={PageSectionVariants.light}>
+        <TextContent>
+          <Text component="h1">Get Started with Resources</Text>
+          <Text component="h3" className="subtitle">
+            Explore a Wide Range of Resources that Resource Hub Offers to
+            Automate and Expedite Your Workflow
+          </Text>
+        </TextContent>
+      </PageSection>
+      <PageSection variant={PageSectionVariants.default}>
+        <Grid hasGutter md={6} lg={3}>
+          {dashboardItems.map((item) => (
+            <GridItem>
+              <ResourcesDashboardCard
+                logoLink={item.logoLink}
+                logoAlt={item.logoAlt}
+                body={item.body}
+                footerLinks={item.footerLinks}
+              />
+            </GridItem>
+          ))}
+        </Grid>
+      </PageSection>
+    </>
+  );
+};
+
+export default ResourcesDashboard;
