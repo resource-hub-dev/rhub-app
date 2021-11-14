f0466ecee64ff3968b0a42c37e4fd82f9f763fbd
Tuan Anh Nguyen <tuanguye@redhat.com>
2021-11-14 16:43:52 +0100

    Add actions

---
+++ src/store/ducks/lab/region/actions.ts
@@ -0,0 +1,50 @@
+import { action } from 'typesafe-actions';
+import {
+  LabRegionTypes,
+  LabRegionData,
+  LabRegionCreate,
+  LabRegionUpdate,
+} from './types';
+import { Error } from '../types';
+
+export const loadRequest = (
+  regionId: number | 'all',
+  parameters?: { [key: string]: string | number }
+) =>
+  action(LabRegionTypes.LOAD_REQUEST, {
+    regionId,
+    parameters: parameters || {},
+  });
+
+export const loadSuccess = (
+  regionId: number,
+  data: LabRegionData | LabRegionData[]
+) => action(LabRegionTypes.LOAD_SUCCESS, { regionId, data });
+
+export const loadFailure = (err: Error) =>
+  action(LabRegionTypes.LOAD_FAILURE, err);
+
+export const createRequest = (payload: LabRegionCreate) =>
+  action(LabRegionTypes.CREATE_REQUEST, payload);
+
+export const createSuccess = () => action(LabRegionTypes.CREATE_SUCCESS);
+
+export const createFailure = (err: Error) =>
+  action(LabRegionTypes.CREATE_FAILURE, err);
+
+export const updateRequest = (regionId: number, data: LabRegionUpdate) =>
+  action(LabRegionTypes.UPDATE_REQUEST, { regionId, data });
+
+export const updateSuccess = () => action(LabRegionTypes.UPDATE_SUCCESS);
+
+export const updateFailure = (err: Error) =>
+  action(LabRegionTypes.UPDATE_FAILURE, err);
+
+export const deleteRequest = (regionId: number) =>
+  action(LabRegionTypes.DELETE_REQUEST, { regionId });
+
+export const deleteSuccess = (regionId: number) =>
+  action(LabRegionTypes.DELETE_SUCCESS, { regionId });
+
+export const deleteFailure = (err: Error) =>
+  action(LabRegionTypes.DELETE_FAILURE, err);
