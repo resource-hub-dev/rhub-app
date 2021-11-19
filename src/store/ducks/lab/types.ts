f53ced5af5bcbe3d80f5aa8a667cc53ddff90d94
Tuan Anh Nguyen <tuanguye@redhat.com>
2021-11-19 18:34:24 +0100

    Refactor Error

--- src/store/ducks/lab/types.ts
+++ src/store/ducks/lab/types.ts
@@ -1,8 +1,15 @@
 export interface Quota {
   // Tower types
-  id: number;
+  id?: number;
   num_vcpus: number;
   ram_mb: number;
   num_volumes: number;
   volumes_gb: number;
+}
+
+export interface Error {
+  detail: string;
+  status: number;
+  title: string;
+  type: string;
 }
