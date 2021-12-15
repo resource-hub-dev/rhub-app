ccd50a2aca617af1466eb4b2eef07a67648db33f
Charles Thao <cthao@redhat.com>
2021-12-15 16:58:52 -0500

    fix: add types and styling to comply with TS4

--- src/store/ducks/cowsay/sagas.ts
+++ src/store/ducks/cowsay/sagas.ts
@@ -4,7 +4,7 @@ import { loadSuccess, loadFailure } from './actions';
 
 export default function* load() {
   try {
-    const response = yield call(api.get, '/cowsay');
+    const response: Record<string, any> = yield call(api.get, '/cowsay');
     yield put(loadSuccess(response.data));
   } catch (err) {
     yield put(loadFailure());
