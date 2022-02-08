bac7cd9e75f798bc7ec03cd464aea8a27aacd015
Charles Thao <cthao@redhat.com>
2022-02-08 13:45:33 -0500

    feat: Add mocks and tests for lab/cluster

---
+++ __tests__/store/ducks/lab/cluster/reducer.test.ts
@@ -0,0 +1,193 @@
+import reducer, { INITIAL_STATE } from '@ducks/lab/cluster';
+import * as actions from '@ducks/lab/cluster/actions';
+
+import * as mocks from '@mocks/labCluster';
+
+describe('cluster reducer', () => {
+  test('returns the initial state', () => {
+    expect(reducer(undefined, { type: 'stub' })).toEqual(INITIAL_STATE);
+  });
+  test('handles load request', () => {
+    expect(reducer(INITIAL_STATE, actions.loadRequest(1))).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(reducer(INITIAL_STATE, actions.loadEventRequest(1))).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(reducer(INITIAL_STATE, actions.loadHostRequest(1))).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(reducer(INITIAL_STATE, actions.loadStdoutRequest(123))).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(
+      reducer(
+        INITIAL_STATE,
+        actions.updateRequest(1, {
+          reservation_expiration: null,
+          lifespan_expiration: null,
+        })
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(reducer(INITIAL_STATE, actions.deleteRequest(1))).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+    expect(
+      reducer(
+        INITIAL_STATE,
+        actions.createClusterRequest(mocks.clusterCreateData)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      loading: true,
+      error: false,
+    });
+  });
+  test('handles load success for a single cluster', () => {
+    const apiResponseData = mocks.clusterExample;
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadSuccess(mocks.clusterExample.id, apiResponseData)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      data: { [mocks.clusterExample.id]: mocks.loadedCluster },
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for checking if cluster exists', () => {
+    const apiResponseData = [mocks.clusterExample];
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadSuccess('all', apiResponseData, true)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      clusterExists: true,
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for all cluster', () => {
+    const apiResponseData = [mocks.clusterExample];
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadSuccess('all', apiResponseData)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      data: { [mocks.clusterExample.id]: mocks.loadedCluster },
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for events of a cluster', () => {
+    const apiResponse = mocks.clusterEventsData;
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadEventSuccess(apiResponse)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      events: mocks.loadedEventsState,
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for hosts of a cluster', () => {
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadHostSuccess(1, mocks.clusterHosts)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      data: {
+        1: {
+          hosts: mocks.clusterHosts,
+        },
+      },
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for stdout of a cluster', () => {
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadStdoutSuccess('string')
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      stdOutput: 'string',
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles load success for stdout of a cluster', () => {
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.loadStdoutSuccess('string')
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      stdOutput: 'string',
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles update success for a cluster', () => {
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.updateSuccess(mocks.clusterExample)
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      data: { [mocks.clusterExample.id]: mocks.loadedCluster },
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles delete success for a cluster', () => {
+    expect(
+      reducer({ ...INITIAL_STATE, loading: true }, actions.deleteSuccess(1))
+    ).toEqual({
+      ...INITIAL_STATE,
+      loading: false,
+      error: false,
+    });
+  });
+  test('handles create success for a cluster', () => {
+    expect(
+      reducer(
+        { ...INITIAL_STATE, loading: true },
+        actions.createClusterSuccess()
+      )
+    ).toEqual({
+      ...INITIAL_STATE,
+      loading: false,
+      error: false,
+    });
+  });
+});
