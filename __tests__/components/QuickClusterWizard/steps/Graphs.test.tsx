3b9c0d058683d099dcd9de0ddb4b2ab072bdd064
Charles Thao <cthao@redhat.com>
2022-06-27 13:29:33 -0500

    refactor: refactor tests for changes in @ducks/lab/region

--- __tests__/components/QuickClusterWizard/steps/Graphs.test.tsx
+++ __tests__/components/QuickClusterWizard/steps/Graphs.test.tsx
@@ -5,11 +5,13 @@ import { connectedRender } from '@tests/testUtils';
 import GraphsUtilization from '@components/QuickClusterWizard/steps/Graphs';
 
 import * as mocks from '@mocks/QuickClusterWizard';
+import { labProductExample } from '@mocks/labProduct';
 
 describe('<GraphsUtilization />', () => {
   test('Renders graphs', async () => {
     const { result } = connectedRender(
       <GraphsUtilization
+        regionId={labProductExample.id}
         vCPUCoreQuota={10}
         ramMbQuota={32 * 1024}
         volumesGbQuota={128}
@@ -53,6 +55,7 @@ describe('<GraphsUtilization />', () => {
   test('Renders without usage', async () => {
     const { result } = connectedRender(
       <GraphsUtilization
+        regionId={labProductExample.id}
         vCPUCoreQuota={10}
         ramMbQuota={32 * 1024}
         volumesGbQuota={128}
