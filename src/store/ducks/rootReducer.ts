86ce1f1908eef757da54e897e78842ed82ddd59b
Charles Thao <cthao@redhat.com>
2022-08-02 14:59:29 -0400

    fix: add labLocation, openstackCloud, openstackProject to AppState and rootReducer

--- src/store/ducks/rootReducer.ts
+++ src/store/ducks/rootReducer.ts
@@ -5,6 +5,7 @@ import labPolicy from './lab/policy';
 import labProduct from './lab/product';
 import cluster from './lab/cluster';
 import labRegion from './lab/region';
+import labLocation from './lab/location';
 import openstackCloud from './openstack/cloud';
 import openstackProject from './openstack/project';
 
@@ -15,6 +16,7 @@ export default combineReducers({
   labPolicy,
   labRegion,
   labProduct,
+  labLocation,
   openstackCloud,
   openstackProject,
 });
