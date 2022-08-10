8f6c10d6dd7861fa52350ee6d668bf6658507e4c
Charles Thao <cthao@redhat.com>
2022-08-10 12:25:03 -0500

    feat: moved ScheduleAvailability

---
+++ src/components/policies/formField/ScheduleAvailability.tsx
@@ -0,0 +1,54 @@
+import { Grid, GridItem, TextInput } from '@patternfly/react-core';
+import React from 'react';
+import { useFormContext } from 'react-hook-form';
+import ControlledField from './ControlledField';
+
+interface Props {
+  watchSchedAvailEnabled: boolean;
+  currentDate: string;
+}
+const ScheduledAvailability: React.FC<Props> = ({
+  watchSchedAvailEnabled,
+  currentDate,
+}: Props) => {
+  const { register } = useFormContext();
+  return (
+    <ControlledField
+      label="Scheduled Availability"
+      switchId="sched-avail-switch"
+      controllerName="constraintsEnabled.schedAvail"
+      fieldId="sched-avail"
+    >
+      <Grid hasGutter md={6} sm={12}>
+        <GridItem>
+          <div>From</div>
+          <TextInput
+            {...register('schedAvailFrom', {
+              disabled: !watchSchedAvailEnabled,
+            })}
+            isDisabled={!watchSchedAvailEnabled}
+            type="date"
+            aria-label="sched-avail-from"
+            defaultValue={currentDate}
+            onChange={() => undefined}
+          />
+        </GridItem>
+        <GridItem>
+          <div>To</div>
+          <TextInput
+            {...register('schedAvailTo', {
+              disabled: !watchSchedAvailEnabled,
+            })}
+            isDisabled={!watchSchedAvailEnabled}
+            type="date"
+            aria-label="sched-avail-to"
+            defaultValue={currentDate}
+            onChange={() => undefined}
+          />
+        </GridItem>
+      </Grid>
+    </ControlledField>
+  );
+};
+
+export default ScheduledAvailability;
